9cdd95caf33d2c0a239fedb6d2b2432a18b25f55
Srchenko <Srchenko@SRCHENKO-PC>
2016-10-18 23:36:46 -0300

    a

--- programa1/programa1/Program.cs
+++ programa1/programa1/Program.cs
@@ -14,7 +14,7 @@ namespace programa1
         [STAThread]
         static void Main()
         {
-            //Maxi cagón
+            //Maxi cagóna
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
