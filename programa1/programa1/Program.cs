8cda5c1ee873f8506d3db9ea2c984456467b58f9
Srchenko <Srchenko@SRCHENKO-PC>
2016-10-19 19:36:00 -0300

    asd

--- programa1/programa1/Program.cs
+++ programa1/programa1/Program.cs
@@ -14,7 +14,7 @@ namespace programa1
         [STAThread]
         static void Main()
         {
-            //Maxi me succiona el 
+            //Maxi me succiona el seba
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
