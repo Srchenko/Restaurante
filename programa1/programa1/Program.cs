39239a6063b9598b497a59c78c09043f432ad6c7
Srchenko <Srchenko@SRCHENKO-PC>
2016-10-18 23:34:10 -0300

    agregué un maxi cagon en el codigo

--- programa1/programa1/Program.cs
+++ programa1/programa1/Program.cs
@@ -14,6 +14,7 @@ namespace programa1
         [STAThread]
         static void Main()
         {
+            //Maxi cagón
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
