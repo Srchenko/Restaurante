848b763cfd1c831bfddbc59d504a7aa2ef895f7e
Srchenko <Srchenko@SRCHENKO-PC>
2016-10-18 23:49:19 -0300

    agregue cosas que me puedo arrepentir

--- programa1/programa1/Program.cs
+++ programa1/programa1/Program.cs
@@ -14,7 +14,7 @@ namespace programa1
         [STAThread]
         static void Main()
         {
-            //Maxi cagóna
+            //Maxi me succiona el escroto
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
