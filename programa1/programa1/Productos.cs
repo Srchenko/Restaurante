1653ba501d9ce0b3a7c96c413c653ab615615a8c
Seba <Seba@SEBA-PC>
2018-01-09 11:21:31 -0300

    botones eliminar y modificar

--- programa1/programa1/Productos.cs
+++ programa1/programa1/Productos.cs
@@ -70,6 +70,43 @@ namespace programa1
             id_producto = 0;
         }
 
+        // *****************debe verificar si es la primer pestaña 
+        private bool validacion_copada1()
+        {
+            string error = "";
+            //primero se verifica si hay textboxs vacios
+            if (this.Controls.OfType<TextBox>().Any(t => string.IsNullOrEmpty(t.Text)))
+
+            {
+                MessageBox.Show("Faltan completar campos", "Atención");
+                return false;
+            }
+            //si no hay textboxs vacios entonces se pasa por una serie de validaciones para  tratar de tener mas datos correctos
+            else
+            {
+
+                if (txt_descripcion.Text.Length < 3)
+                {
+                    error += "Nombre de producto muy corto. ";
+                }
+
+                if (float.Parse(txt_precio.Text) < 0)
+                {
+                    error += "Ingrese un precio mayor. ";
+                }
+     
+            }
+            if (error != "")
+            {
+                MessageBox.Show(error, "Atención");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
         //se ponen las categorias en un combobox
         private void cargarListaCategorias()
         {
@@ -203,9 +240,48 @@ namespace programa1
             limpiarTexto();
         }
 
+
+
         //segunda pestaña
 
         int id_materia_prima = 0;
+
+        // *****************debe verificar si es la segunda pestaña 
+        private bool validacion_copada2()
+        {
+            string error = "";
+            //primero se verifica si hay textboxs vacios
+            if (this.Controls.OfType<TextBox>().Any(t => string.IsNullOrEmpty(t.Text)))
+
+            {
+                MessageBox.Show("Faltan completar campos", "Atención");
+                return false;
+            }
+            //si no hay textboxs vacios entonces se pasa por una serie de validaciones para  tratar de tener mas datos correctos
+            else
+            {
+
+                if (txt_descripcion.Text.Length < 3)
+                {
+                    error += "Nombre de producto muy corto. ";
+                }
+
+                if (float.Parse(txt_precio.Text) < 0)
+                {
+                    error += "Ingrese un precio mayor. ";
+                }
+
+            }
+            if (error != "")
+            {
+                MessageBox.Show(error, "Atención");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
 
         //carga dgv de materias primas
         private void cargarGridMateriasPrimas()
@@ -330,9 +406,48 @@ namespace programa1
             limpiarTexto();
         }
 
+
+
         //tercera pestaña / marcas
 
         int id_marca = 0;
+
+        // *****************debe verificar si es la tercer pestaña 
+        private bool validacion_copada3()
+        {
+            string error = "";
+            //primero se verifica si hay textboxs vacios
+            if (this.Controls.OfType<TextBox>().Any(t => string.IsNullOrEmpty(t.Text)))
+
+            {
+                MessageBox.Show("Faltan completar campos", "Atención");
+                return false;
+            }
+            //si no hay textboxs vacios entonces se pasa por una serie de validaciones para  tratar de tener mas datos correctos
+            else
+            {
+
+                if (txt_descripcion.Text.Length < 3)
+                {
+                    error += "Nombre de producto muy corto. ";
+                }
+
+                if (float.Parse(txt_precio.Text) < 0)
+                {
+                    error += "Ingrese un precio mayor. ";
+                }
+
+            }
+            if (error != "")
+            {
+                MessageBox.Show(error, "Atención");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
 
         //carga dgv de marcas
         private void cargarGridMarcas()
@@ -365,11 +480,193 @@ namespace programa1
         {
             limpiarTexto();
         }
+
+        private void b_agregar3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (validacion_copada3() == false)
+                {
+                    return;
+                }
+                conexion.Open();
+                //Antes de agregar una marca, se intenta que no existan dos marcas iguales
+                SqlCommand comando3 = new SqlCommand("SELECT * FROM Marcas WHERE nombre_marca=@Marca", conexion);
+                comando3.Parameters.Add("@Marca", SqlDbType.Int);
+                comando3.Parameters["@Marca"].Value = txt_marca.Text;
+                SqlDataReader datos3 = comando3.ExecuteReader();
+                if (datos3.Read())
+                {
+                    MessageBox.Show("Esta marca ya existe.", "Atención");
+                    datos3.Close();
+                    conexion.Close();
+                    return;
+                }
+                datos3.Close();
+                //Si la marca no existe, se agrega en la base de datos
+                string sql = "INSERT INTO Marcas(nombre_marca, baja) VALUES (@Nombre,0)";
+                SqlCommand comando = new SqlCommand(sql, conexion);
+                comando.Parameters.Add("@Nombre", SqlDbType.VarChar);
+                comando.Parameters["@Nombre"].Value = txt_marca.Text;
+                comando.ExecuteNonQuery();
+
+                conexion.Close();
+                cargarGridMarcas();
+                limpiarTexto();
+                MessageBox.Show("Se ingresó el dato.", "Atención");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+        }
+
+        private void b_modificar3_Click(object sender, EventArgs e)
+        {
+            if (id_marca != 0)
+            {
+
+                try
+                {
+                    if (validacion_copada3() == false)
+                    {
+                        return;
+                    }
+
+                    conexion.Open();
+                    //al modificar la marca se verifica que no exista ya
+                    SqlCommand comando4 = new SqlCommand("SELECT * FROM Marcas WHERE id_marca=@ID", conexion);
+                    comando4.Parameters.Add("@ID", SqlDbType.Int);
+                    comando4.Parameters["@ID"].Value = id_marca;
+                    SqlDataReader datos4 = comando4.ExecuteReader();
+                    string marcaor = "";
+                    if (datos4.Read())
+                    {
+                        marcaor = (datos4["nombre_marca"]).ToString();
+                    }
+                    datos4.Close();
+
+                    SqlCommand comando5 = new SqlCommand("SELECT * FROM Marca WHERE nombre_marca<>@Marca", conexion);
+                    comando5.Parameters.Add("@Marca", SqlDbType.Int);
+                    comando5.Parameters["@Marca"].Value = marcaor;
+                    SqlDataReader datos5 = comando5.ExecuteReader();
+                    String marcaref = "";
+                    while (datos5.Read())
+                    {
+                        marcaref = datos5["nombre_marca"].ToString();
+                        if (txt_marca.Text.Equals(marcaref))
+                        {
+                            MessageBox.Show("Esta Marca ya existe.", "Atención");
+                            datos5.Close();
+                            conexion.Close();
+                            return;
+                        }
+                    }
+                    datos5.Close();
+                    //si el nombre esta bien, se modifica la marca
+                    string sql = "UPDATE Marcas SET nombre_marca=@Nombre, WHERE id_marca=@ID";
+                    SqlCommand comando = new SqlCommand(sql, conexion);
+                    comando.Parameters.Add("@ID", SqlDbType.Int);
+                    comando.Parameters["@ID"].Value = id_marca;
+                    comando.Parameters.Add("@Nombre", SqlDbType.VarChar);
+                    comando.Parameters["@Nombre"].Value = txt_marca.Text;
+
+                    comando.ExecuteNonQuery();
+
+                    conexion.Close();
+                    cargarGridMarcas();
+                    MessageBox.Show("Se modificó el dato.", "Atención");
+                    limpiarTexto();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una fila.", "Atención");
+            }
+        }
+
+        private void b_eliminar3_Click(object sender, EventArgs e)
+        {
+            if (id_marca != 0)
+            {
+                //Se pregunta si de verdad quiere eliminar una marca... 
+                if (MessageBox.Show("¿Está seguro?", "Eliminar", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        //las marcas eliminados siguen estando en la tabla de la base de datos, pero no se muestran como activos
+                        conexion.Open();
+                        string sql = "UPDATE Marcas SET baja=1 WHERE id_marca=@ID";
+                        SqlCommand comando = new SqlCommand(sql, conexion);
+                        comando.Parameters.Add("@ID", SqlDbType.Int);
+                        comando.Parameters["@ID"].Value = id_marca;
+                        comando.ExecuteNonQuery();
+
+                        conexion.Close();
+                        cargarGridMarcas();
+                        limpiarTexto();
+                        MessageBox.Show("Se eliminó la marca.", "Atención");
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Error.", "Atención");
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una fila.", "Atención");
+            }
+        }
+
+
+
         //tercera pestaña / categorias
         int id_categoria = 0;
 
-        //carga dgv de marcas
-        private void cargarGridCategoria()
+        // *****************debe verificar si es la primer pestaña 
+        private bool validacion_copada4()
+        {
+            string error = "";
+            //primero se verifica si hay textboxs vacios
+            if (this.Controls.OfType<TextBox>().Any(t => string.IsNullOrEmpty(t.Text)))
+
+            {
+                MessageBox.Show("Faltan completar campos", "Atención");
+                return false;
+            }
+            //si no hay textboxs vacios entonces se pasa por una serie de validaciones para  tratar de tener mas datos correctos
+            else
+            {
+
+                if (txt_categoria.Text.Length < 3)
+                {
+                    error += "Nombre de categoria muy corto. ";
+                }
+
+            }
+            if (error != "")
+            {
+                MessageBox.Show(error, "Atención");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        //carga dgv de categorias
+        private void cargarGridCategorias()
         {
             try
             {
@@ -399,6 +696,154 @@ namespace programa1
         {
             limpiarTexto();
         }
+
+        private void b_agregar4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (validacion_copada4() == false)
+                {
+                    return;
+                }
+                conexion.Open();
+                //Antes de agregar una categoria, se intenta que no existan dos categorias iguales
+                SqlCommand comando3 = new SqlCommand("SELECT * FROM Categorias WHERE nombre_categoria=@Categoria", conexion);
+                comando3.Parameters.Add("@Categoria", SqlDbType.Int);
+                comando3.Parameters["@Categoria"].Value = txt_categoria.Text;
+                SqlDataReader datos3 = comando3.ExecuteReader();
+                if (datos3.Read())
+                {
+                    MessageBox.Show("Esta categoría ya existe.", "Atención");
+                    datos3.Close();
+                    conexion.Close();
+                    return;
+                }
+                datos3.Close();
+                //Si la categoria no existe, se agrega en la base de datos
+                string sql = "INSERT INTO Categorias(nombre_categoria, baja) VALUES (@Nombre,0)";
+                SqlCommand comando = new SqlCommand(sql, conexion);
+                comando.Parameters.Add("@Nombre", SqlDbType.VarChar);
+                comando.Parameters["@Nombre"].Value = txt_categoria.Text;
+                comando.ExecuteNonQuery();
+
+                conexion.Close();
+                cargarGridMarcas();
+                limpiarTexto();
+                MessageBox.Show("Se ingresó el dato.", "Atención");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+        }
+
+        private void b_modificar4_Click(object sender, EventArgs e)
+        {
+            if (id_categoria != 0)
+            {
+
+                try
+                {
+                    if (validacion_copada4() == false)
+                    {
+                        return;
+                    }
+
+                    conexion.Open();
+                    //al modificar la categoria se verifica que no exista ya
+                    SqlCommand comando4 = new SqlCommand("SELECT * FROM Categorias WHERE id_categoria=@ID", conexion);
+                    comando4.Parameters.Add("@ID", SqlDbType.Int);
+                    comando4.Parameters["@ID"].Value = id_categoria;
+                    SqlDataReader datos4 = comando4.ExecuteReader();
+                    string categoriaor = "";
+                    if (datos4.Read())
+                    {
+                        categoriaor = (datos4["nombre_categoria"]).ToString();
+                    }
+                    datos4.Close();
+
+                    SqlCommand comando5 = new SqlCommand("SELECT * FROM Categorias WHERE nombre_categoria<>@Categoria", conexion);
+                    comando5.Parameters.Add("@Categoria", SqlDbType.Int);
+                    comando5.Parameters["@Categoria"].Value = categoriaor;
+                    SqlDataReader datos5 = comando5.ExecuteReader();
+                    String categoriaref = "";
+                    while (datos5.Read())
+                    {
+                        categoriaref = datos5["nombre_categoria"].ToString();
+                        if (txt_marca.Text.Equals(categoriaref))
+                        {
+                            MessageBox.Show("Esta Categoría ya existe.", "Atención");
+                            datos5.Close();
+                            conexion.Close();
+                            return;
+                        }
+                    }
+                    datos5.Close();
+                    //si el nombre esta bien, se modifica la categoria
+                    string sql = "UPDATE Categorias SET nombre_categoria=@Nombre, WHERE id_categoria=@ID";
+                    SqlCommand comando = new SqlCommand(sql, conexion);
+                    comando.Parameters.Add("@ID", SqlDbType.Int);
+                    comando.Parameters["@ID"].Value = id_marca;
+                    comando.Parameters.Add("@Nombre", SqlDbType.VarChar);
+                    comando.Parameters["@Nombre"].Value = txt_categoria.Text;
+
+                    comando.ExecuteNonQuery();
+
+                    conexion.Close();
+                    cargarGridMarcas();
+                    MessageBox.Show("Se modificó el dato.", "Atención");
+                    limpiarTexto();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una fila.", "Atención");
+            }
+        }
+
+        private void b_eliminar4_Click(object sender, EventArgs e)
+        {
+            if (id_marca != 0)
+            {
+                //Se pregunta si de verdad quiere eliminar una categoria... 
+                if (MessageBox.Show("¿Está seguro?", "Eliminar", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        //las marcas eliminados siguen estando en la tabla de la base de datos, pero no se muestran como activos
+                        conexion.Open();
+                        string sql = "UPDATE Categorias SET baja=1 WHERE id_categoria=@ID";
+                        SqlCommand comando = new SqlCommand(sql, conexion);
+                        comando.Parameters.Add("@ID", SqlDbType.Int);
+                        comando.Parameters["@ID"].Value = id_categoria;
+                        comando.ExecuteNonQuery();
+
+                        conexion.Close();
+                        cargarGridCategorias();
+                        limpiarTexto();
+                        MessageBox.Show("Se eliminó la categoria.", "Atención");
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Error.", "Atención");
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una fila.", "Atención");
+            }
+        }
+
 
         public Productos()
         {
